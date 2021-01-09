5c5e10fb16fd1adc6ddfe6123597e78dbb197979
Conor Stuart Roe <cstuartroe@haverford.edu>
2021-01-09 14:13:19 -0500

    First commit

---
+++ flower_utils.py
@@ -0,0 +1,89 @@
+import math
+
+
+def star_peaks_valleys(points, center, peak_radius, valley_radius, askew):
+    peaks, valleys = [], []
+
+    for i in range(points):
+        peak_theta = math.pi*(2*i/points + 3/2 + (1/points if askew else 0))
+        valley_theta = peak_theta + math.pi/points
+        center_x, center_y = center
+
+        peaks.append((
+            center_x + peak_radius * math.cos(peak_theta),
+            center_y + peak_radius * math.sin(peak_theta),
+        ))
+
+        valleys.append((
+            center_x + valley_radius * math.cos(valley_theta),
+            center_y + valley_radius * math.sin(valley_theta)
+        ))
+
+    return peaks, valleys
+
+
+def star_vertices(points, center, peak_radius, valley_radius, askew):
+    peaks, valleys = star_peaks_valleys(points, center, peak_radius, valley_radius, askew)
+    out = []
+    for i in range(len(peaks)):
+        out += peaks[i], valleys[i]
+    return out
+
+
+def flat_arm_valley_radius(points, peak_radius):
+    return peak_radius * math.cos(2 * math.pi / points) / math.cos(math.pi / points)
+
+
+def flat_armed_star_peaks_valleys(points, center, peak_radius, askew):
+    return star_peaks_valleys(points, center, peak_radius, flat_arm_valley_radius(points, peak_radius), askew)
+
+
+def flat_armed_star_vertices(points, center, peak_radius, askew):
+    return star_vertices(points, center, peak_radius, flat_arm_valley_radius(points, peak_radius), askew)
+
+
+def polygon_path(vertices):
+    vertex_string = ""
+    for i, (x, y) in enumerate(vertices):
+        vertex_string += "M " if (i == 0) else "L "
+        vertex_string += f"{x} {y} "
+
+    return vertex_string + "Z"
+
+
+def flower_control_points(center, peak, valley, curviness):
+    center_x, center_y = center
+    peak_x, peak_y = peak
+    valley_x, valley_y = valley
+
+    direction_vector = (
+        (center_x + valley_x)/2 - peak_x,
+        (center_y + valley_y)/2 - peak_y,
+    )
+
+    outer_control = (
+        peak_x + direction_vector[0]*curviness,
+        peak_y + direction_vector[1]*curviness,
+    )
+
+    inner_control = (
+        valley_x - direction_vector[0]*curviness,
+        valley_y - direction_vector[1]*curviness,
+    )
+
+    return outer_control, inner_control
+
+
+def flower_path(peaks, valleys, center, curviness=1/6):
+    vertex_string = f"M {peaks[0][0]} {peaks[0][1]}"
+
+    for i in range(len(peaks)):
+        source_peak, valley, dest_peak = peaks[i], valleys[i], peaks[(i+1) % len(peaks)]
+
+        (control1_x, control1_y), (control2_x, control2_y) = flower_control_points(center, source_peak, valley, curviness)
+        vertex_string += f"C {control1_x} {control1_y}, {control2_x} {control2_y}, {valley[0]} {valley[1]} "
+
+        (control2_x, control2_y), (control1_x, control1_y) = flower_control_points(center, dest_peak, valley, curviness)
+        vertex_string += f"C {control1_x} {control1_y}, {control2_x} {control2_y}, {dest_peak[0]} {dest_peak[1]} "
+
+    return vertex_string + "Z"
---
+++ svg_gen.py
@@ -0,0 +1,141 @@
+from flower_utils import *
+
+
+def path_template(vertex_string, color):
+    return f'<path d="{vertex_string}" fill="{color}"/>'
+
+
+def svg_template(height, width, *paths):
+    return f'<svg height="{height}" width="{width}">\n' + "\n".join(paths) + '\n</svg>'
+
+
+def create_rasta_flower():
+    width, height = 1000, 1000
+    center = width/2, height/2
+
+    stars = list(map(lambda x: path_template(
+        flower_path(
+            *star_peaks_valleys(
+                6,
+                center,
+                x[0],
+                flat_arm_valley_radius(6, x[0]),
+                False,
+            ),
+            center,
+            curviness=1/3
+        ),
+        x[1]
+    ), [
+        (500, "black"),
+        (400, "#007f00"),
+        (300, "#ffff00"),
+        (200, "#7f0000"),
+    ]))
+
+    with open("images/rasta_star.svg", "w") as fh:
+        fh.write(
+            svg_template(
+                width,
+                height,
+                *stars,
+            )
+        )
+
+
+def create_trippy_lotus():
+    width, height = 1000, 1000
+    center = width / 2, height / 2
+
+    stars = list(map(lambda x: path_template(
+        flower_path(
+            *star_peaks_valleys(
+                8,
+                center,
+                x[1][0],
+                flat_arm_valley_radius(8, x[1][0]),
+                x[0] % 2 == 1,
+            ),
+            center,
+            curviness=1/4
+        ),
+        x[1][1]
+    ), enumerate([
+        (500, "black"),
+        (383, "#007f00"),
+        (292, "#ffff00"),
+        (220, "#7f0000"),
+        (160, "#3b0082"),
+    ])))
+
+    with open("images/trippy_lotus.svg", "w") as fh:
+        fh.write(
+            svg_template(
+                width,
+                height,
+                *stars,
+            )
+        )
+
+
+def create_black_lotus():
+    width, height = 1000, 1000
+    center = width / 2, height / 2
+
+    path = path_template(
+        flower_path(
+            *star_peaks_valleys(
+                8,
+                center,
+                500,
+                flat_arm_valley_radius(8, 500),
+                False,
+            ),
+            center,
+        ),
+        "black",
+    )
+
+    with open("images/black_lotus.svg", "w") as fh:
+        fh.write(
+            svg_template(
+                width,
+                height,
+                path,
+            )
+        )
+
+
+def create_rub_el_hizb_black_solid():
+    width, height = 1000, 1000
+    center = width/2, height/2
+    radius = 500
+
+    path = path_template(
+        polygon_path(
+            star_vertices(
+                8,
+                center,
+                radius,
+                flat_arm_valley_radius(8, radius),
+                False,
+            ),
+        ),
+        "black",
+    )
+
+    with open("images/rub_el_hizb_solid.svg", "w") as fh:
+        fh.write(
+            svg_template(
+                width,
+                height,
+                path,
+            )
+        )
+
+
+if __name__ == "__main__":
+    create_rasta_flower()
+    create_trippy_lotus()
+    create_black_lotus()
+    create_rub_el_hizb_black_solid()
