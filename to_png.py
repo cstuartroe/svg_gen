eadc931e84e5407250532c8a5b84a30162c9a51a
Conor Stuart-Roe <conorstuartroe@gmail.com>
2025-01-03 15:23:12 -0500

    Use inkscape because cairosvg can't handle masks

--- to_png.py
+++ to_png.py
@@ -1,13 +1,14 @@
-import cairosvg
 import os
+import subprocess
+
+from tqdm import tqdm
+
 
 for root, _, files in os.walk("images"):
     new_dir = root.replace("images", "pngs")
     os.makedirs(new_dir, exist_ok=True)
-    for filename in files:
+    print(root)
+    for filename in tqdm(files):
         if filename.endswith(".svg"):
             new_filename = filename.replace(".svg", ".png")
-            cairosvg.svg2png(
-                url=os.path.join(root, filename),
-                write_to=os.path.join(new_dir, new_filename),
-            )
+            subprocess.run(['/Applications/Inkscape.app/Contents/MacOS/inkscape', f'--export-filename={os.path.join(new_dir, new_filename)}', os.path.join(root, filename)])
